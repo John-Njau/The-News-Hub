8ba7bed29ea199174a63b055e8e528409ffd0f88
john.njau <john.njoroge@student.moringaschool.com>
2022-05-02 20:12:15 +0300

    ft: tests function in manage.py

--- manage.py
+++ manage.py
@@ -7,6 +7,13 @@ app = create_app('production')
 manager = Manager(app)
 manager.add_command('server',Server)
 
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
 manager.add_command('server',Server)
 if __name__ == '__main__':
     manager.run()
